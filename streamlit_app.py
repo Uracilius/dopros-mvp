d11f7d4ad1794b82ef7ae3ba813a68ed4e4ede1e
Uracilius <uracilmabiyev@gmail.com>
2025-03-03 13:14:39 +0500

    modify: removed watcher

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from face_analysis_main import FaceAnalysisPipeline
 import openai
 
+st.set_option('server.runOnSave', False)
 # Настройка страницы
 st.set_page_config(
     page_title="Анализ лиц в видео",
